54325770a7274dbdb031903d40c13b1462e4dc5b
Aislan Santos <aislan.santos@gmail.com>
2024-04-02 11:08:42 -0300

    Iniciado o arquivo do service criaando as funções que vão ser usadas para os processos de registro, login e listar os users do sistema.

--- src/services/User.Service.ts
+++ src/services/User.Service.ts
@@ -1,2 +1,21 @@
 import { User } from "../models/User";
 
+export const createUser = async (email: string, password: string) => {
+    const hasUser = await User.findOne({ where: { email } });
+    if (!hasUser) {
+        
+    }
+    return new Error("E-mail já existe.")
+}
+
+export const findByEmail = async (email: string) => {
+
+}
+
+export const matchPassword = async (password: string, encrypted: string) => {
+
+}
+
+export const all = async () => {
+
+}
